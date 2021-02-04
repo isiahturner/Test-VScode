711899c641b5c55ddc76c341ff9d27d3e0d8c6f8
macavelli <54381776+isiahturner@users.noreply.github.com>
2021-02-04 15:36:31 -0500

    first commit

---
+++ inputFileProgram.py
@@ -0,0 +1,14 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("Your full name is ", firstName, lastName)
+for letter in firstName:
+    print(2)
+
+d = 5
+print("hello")
+while d == 5:
+    print("happy")
+    d += 1
+
+print("hello world")
